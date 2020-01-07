57e159366ebb36ef61797cdf49eea82596848a90
sonAjin <sonajin11@gmail.com>
2020-01-07 11:28:44 +0900

    Solved Problem
    solved today problem

--- 20200107/sumBetweenTwoInteger-kotlin-saj/src/main/kotlin/App.kt
+++ 20200107/sumBetweenTwoInteger-kotlin-saj/src/main/kotlin/App.kt
@@ -1,7 +1,7 @@
 fun sumBetweenTwoInteger(a: Int, b: Int): Long {
     return when {
-        a < b -> (a..b).sum().toLong()
-        a > b -> (b..a).sum().toLong()
+        a < b -> (a.toLong()..b.toLong()).sum().toLong()
+        a > b -> (b.toLong()..a.toLong()).sum().toLong()
         else -> a.toLong()
     }
 }
